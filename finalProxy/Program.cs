9b6ab56da3e08b0827fecc5a0bf9fc6f7af76474
idan dagan <daganida@post.bgu.ac.il>
2016-07-26 21:36:09 +0300

    Init project.

---
+++ finalProxy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace HTTPProxyServer
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+           
+
+            if (ProxyServer.Server.Start())
+            {
+                Console.WriteLine(String.Format("Server started on {0}:{1}...Press enter key to end",ProxyServer.Server.ListeningIPInterface,ProxyServer.Server.ListeningPort));
+                Console.ReadLine();
+                Console.WriteLine("Shutting down");
+                ProxyServer.Server.Stop();
+                Console.WriteLine("Server stopped...");
+            }
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
---
+++ finalProxy/ProxyServer.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HTTPProxyServer
+{
+    public sealed class ProxyServer
+    {
+        private static readonly ProxyServer _server = new ProxyServer();
+
+        private static readonly int BUFFER_SIZE = 8192;
+        private static readonly char[] semiSplit = new char[] { ';' };
+        private static readonly char[] equalSplit = new char[] { '=' };
+        private static readonly String[] colonSpaceSplit = new string[] { ": " };
+        private static readonly char[] spaceSplit = new char[] { ' ' };
+        private static readonly char[] commaSplit = new char[] { ',' };
+        private static readonly Regex cookieSplitRegEx = new Regex(@",(?! )");
+        private static X509Certificate2 _certificate;
+        private static object _outputLockObj = new object();
+
+
+        private TcpListener _listener;
+        private Thread _listenerThread;
+        private Thread _cacheMaintenanceThread;
+
+        public IPAddress ListeningIPInterface
+        {
+            get
+            {
+                IPAddress addr = IPAddress.Loopback;
+                return addr;
+            }
+        }
+
+        public Int32 ListeningPort
+        {
+            get
+            {
+                Int32 port = 443;              
+                return port;
+            }
+        }
+
+        private ProxyServer()
+        {
+            _listener = new TcpListener(ListeningIPInterface, ListeningPort);
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        }
+
+
+
+        public static ProxyServer Server
+        {
+            get { return _server; }
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                String certFilePath = "cert.cer";
+                try
+                {
+                    _certificate = new X509Certificate2(certFilePath);
+                }
+                catch (Exception ex)
+                {
+                    throw new ConfigurationErrorsException(String.Format("Certificate from file {0} could not be created.",certFilePath), ex);
+                }
+                _listener.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            _listenerThread = new Thread(new ParameterizedThreadStart(Listen));
+            _listenerThread.Start(_listener);
+
+            return true;
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+
+            //wait for server to finish processing current connections...
+
+            _listenerThread.Abort();
+            _listenerThread.Join();
+            _listenerThread.Join();
+        }
+
+        private static void Listen(Object obj)
+        {
+            TcpListener listener = (TcpListener)obj;
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    while (!ThreadPool.QueueUserWorkItem(new WaitCallback(ProxyServer.ProcessClient), client)) ;
+                }
+            }
+            catch (ThreadAbortException) { }
+            catch (SocketException) { }
+        }
+
+
+        private static void ProcessClient(Object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            try
+            {
+                DoHttpProcessing(client);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private static void DoHttpProcessing(TcpClient client)
+        {
+            Stream clientStream = client.GetStream();
+            Stream outStream = clientStream; //use this stream for writing out - may change if we use ssl
+            SslStream sslStream = null;
+            StreamReader clientStreamReader = new StreamReader(clientStream);
+            try
+            {
+                //read the first line HTTP command
+                String httpCmd = clientStreamReader.ReadLine();
+                if (String.IsNullOrEmpty(httpCmd))
+                {
+                    clientStreamReader.Close();
+                    clientStream.Close();
+                    return;
+                }
+                //break up the line into three components
+                String[] splitBuffer = httpCmd.Split(spaceSplit, 3);
+
+                String method = splitBuffer[0];
+                String remoteUri = splitBuffer[1];
+                Version version = new Version(1, 0);
+
+                HttpWebRequest webReq;
+                HttpWebResponse response = null;
+                if (splitBuffer[0].ToUpper() == "CONNECT")
+                {
+                    if (connectHandler(splitBuffer, ref remoteUri, ref clientStreamReader, ref clientStream, ref sslStream, ref outStream, ref method))
+                        return;
+                }
+
+                //construct the web request that we are going to issue on behalf of the client.
+                webReq = (HttpWebRequest)HttpWebRequest.Create(remoteUri);
+                webReq.Method = method;
+                webReq.ProtocolVersion = version;
+
+                //read the request headers from the client and copy them to our request
+                int contentLen = ReadRequestHeaders(clientStreamReader, webReq);
+                
+                webReq.Proxy = null;
+                webReq.KeepAlive = false;
+                webReq.AllowAutoRedirect = false;
+                webReq.AutomaticDecompression = DecompressionMethods.None;
+                if (method.ToUpper() == "POST")
+                {
+                    char[] postBuffer = new char[contentLen];
+                    int bytesRead;
+                    int totalBytesRead = 0;
+                    StreamWriter sw = new StreamWriter(webReq.GetRequestStream());
+                    while (totalBytesRead < contentLen && (bytesRead = clientStreamReader.ReadBlock(postBuffer, 0, contentLen)) > 0)
+                    {
+                        totalBytesRead += bytesRead;
+                        sw.Write(postBuffer, 0, bytesRead);
+                    }
+
+                    sw.Close();
+                }
+
+                    webReq.Timeout = 15000;
+
+                    try
+                    {
+                        response = (HttpWebResponse)webReq.GetResponse();
+                    }
+                    catch (WebException webEx)
+                    {
+                        response = webEx.Response as HttpWebResponse;
+                    }
+                    if (response != null)
+                    {
+                        List<Tuple<String,String>> responseHeaders = ProcessResponse(response);
+                        StreamWriter myResponseWriter = new StreamWriter(outStream);
+                        Stream responseStream = response.GetResponseStream();
+                        try
+                        {
+                            //send the response status and response headers
+                            WriteResponseStatus(response.StatusCode,response.StatusDescription, myResponseWriter);
+                            WriteResponseHeaders(myResponseWriter, responseHeaders);
+
+                            DateTime? expires = null;
+
+                            Byte[] buffer;
+                            if (response.ContentLength > 0)
+                                buffer = new Byte[response.ContentLength];
+                            else
+                                buffer = new Byte[BUFFER_SIZE];
+
+                            int bytesRead;
+
+                            while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                outStream.Write(buffer, 0, bytesRead);
+
+                            }
+
+
+                            responseStream.Close();
+
+
+                            outStream.Flush();
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        finally
+                        {
+                            responseStream.Close();
+                            response.Close();
+                            myResponseWriter.Close();
+                        }
+                    }
+                
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+
+                clientStreamReader.Close();
+                clientStream.Close();
+                if (sslStream != null)
+                    sslStream.Close();
+                outStream.Close();
+
+            }
+
+        }
+
+        private static bool connectHandler(string[] splitBuffer, ref string remoteUri, ref StreamReader clientStreamReader,
+            ref Stream clientStream, ref SslStream sslStream, ref Stream outStream, ref string method)
+        {
+            string httpCmd;
+//Browser wants to create a secure tunnel
+            //instead = we are going to perform a man in the middle "attack"
+            //the user's browser should warn them of the certification errors however.
+            //Please note: THIS IS ONLY FOR TESTING PURPOSES - you are responsible for the use of this code
+            remoteUri = "https://" + splitBuffer[1];
+            while (!String.IsNullOrEmpty(clientStreamReader.ReadLine())) ;
+            StreamWriter connectStreamWriter = new StreamWriter(clientStream);
+            connectStreamWriter.WriteLine("HTTP/1.0 200 Connection established");
+            connectStreamWriter.WriteLine(String.Format("Timestamp: {0}", DateTime.Now.ToString()));
+            connectStreamWriter.WriteLine("Proxy-agent: matt-dot-net");
+            connectStreamWriter.WriteLine();
+            connectStreamWriter.Flush();
+
+            sslStream = new SslStream(clientStream, false);
+            try
+            {
+                sslStream.AuthenticateAsServer(_certificate, false, SslProtocols.Tls | SslProtocols.Ssl3 | SslProtocols.Ssl2,
+                    true);
+            }
+            catch (Exception)
+            {
+                sslStream.Close();
+                clientStreamReader.Close();
+                connectStreamWriter.Close();
+                clientStream.Close();
+                return true;
+            }
+
+            //HTTPS server created - we can now decrypt the client's traffic
+            clientStream = sslStream;
+            clientStreamReader = new StreamReader(sslStream);
+            outStream = sslStream;
+            //read the new http command.
+            httpCmd = clientStreamReader.ReadLine();
+            if (String.IsNullOrEmpty(httpCmd))
+            {
+                clientStreamReader.Close();
+                clientStream.Close();
+                sslStream.Close();
+                return true;
+            }
+            splitBuffer = httpCmd.Split(spaceSplit, 3);
+            method = splitBuffer[0];
+            remoteUri = remoteUri + splitBuffer[1];
+            return false;
+        }
+
+        private static List<Tuple<String,String>> ProcessResponse(HttpWebResponse response)
+        {
+            String value=null;
+            String header=null;
+            List<Tuple<String, String>> returnHeaders = new List<Tuple<String, String>>();
+            foreach (String s in response.Headers.Keys)
+            {
+                if (s.ToLower() == "set-cookie")
+                {
+                    header = s;
+                    value = response.Headers[s];
+                }
+                else
+                    returnHeaders.Add(new Tuple<String, String>(s, response.Headers[s]));
+            }
+            
+            if (!String.IsNullOrWhiteSpace(value))
+            {
+                response.Headers.Remove(header);
+                String[] cookies = cookieSplitRegEx.Split(value);
+                foreach (String cookie in cookies)
+                    returnHeaders.Add(new Tuple<String, String>("Set-Cookie", cookie));
+
+            }
+            returnHeaders.Add(new Tuple<String, String>("X-Proxied-By", "matt-dot-net proxy"));
+            return returnHeaders;
+        }
+
+        private static void WriteResponseStatus(HttpStatusCode code, String description, StreamWriter myResponseWriter)
+        {
+            String s = String.Format("HTTP/1.0 {0} {1}", (Int32)code, description);
+            myResponseWriter.WriteLine(s);
+
+        }
+
+        private static void WriteResponseHeaders(StreamWriter myResponseWriter, List<Tuple<String,String>> headers)
+        {
+            if (headers != null)
+            {
+                foreach (Tuple<String,String> header in headers)
+                    myResponseWriter.WriteLine(String.Format("{0}: {1}", header.Item1,header.Item2));
+            }
+            myResponseWriter.WriteLine();
+            myResponseWriter.Flush();
+        }
+
+
+
+
+
+        private static int ReadRequestHeaders(StreamReader sr, HttpWebRequest webReq)
+        {
+            String httpCmd;
+            int contentLen = 0;
+            do
+            {
+                httpCmd = sr.ReadLine();
+                if (String.IsNullOrEmpty(httpCmd))
+                    return contentLen;
+                String[] header = httpCmd.Split(colonSpaceSplit, 2, StringSplitOptions.None);
+                switch (header[0].ToLower())
+                {
+                    case "host":
+                        webReq.Host = header[1];
+                        break;
+                    case "user-agent":
+                        webReq.UserAgent = header[1];
+                        break;
+                    case "accept":
+                        webReq.Accept = header[1];
+                        break; 
+                    case "referer":
+                        webReq.Referer = header[1];
+                        break;
+                    case "cookie":
+                        webReq.Headers["Cookie"] = header[1];
+                        break;
+                    case "proxy-connection":
+                    case "connection":
+                    case "keep-alive":
+                        //ignore these
+                        break;
+                    case "content-length":
+                        int.TryParse(header[1], out contentLen);
+                        break;
+                    case "content-type":
+                        webReq.ContentType = header[1];
+                        break;
+                    case "if-modified-since":
+                        String[] sb = header[1].Trim().Split(semiSplit);
+                        DateTime d;
+                        if (DateTime.TryParse(sb[0], out d))
+                            webReq.IfModifiedSince = d;
+                        break;
+                    default:
+                        try
+                        {
+                            webReq.Headers.Add(header[0], header[1]);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(String.Format("Could not add header {0}.  Exception message:{1}", header[0], ex.Message));
+                        }
+                        break;
+                }
+            } while (!String.IsNullOrWhiteSpace(httpCmd));
+            return contentLen;
+        }
+    }
+}
